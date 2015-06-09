370a673a7c02c5b3ef924b26073b39e5e6a174d1
falconraptor <falconraptor@users.noreply.github.com>
2015-06-08 17:54:17 -0700

    Login window created

---
+++ src/main/scala/com/falconraptor/evolution/Evolution.scala
@@ -0,0 +1,15 @@
+package com.falconraptor.evolution
+
+import com.falconraptor.evolution.player.Login
+
+object Evolution {
+  def main(args: Array[String]) {
+    //TODO init globals if needed
+    //TODO create user login system
+    //TODO use both database and file... file for playing and database for multiplayer
+    Login setGUI()
+    //TODO load user's data
+    //TODO init user's data if any else create
+    //TODO start threads if needed
+  }
+}
